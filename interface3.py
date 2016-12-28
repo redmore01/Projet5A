c82a0163673338d040d958b9f209903274d41d30
redmore01 <houssam.elkari@gmail.com>
2016-12-28 22:01:03 +0100

    Go to last interface(video interface)

--- interface3.py
+++ interface3.py
@@ -1,51 +1,97 @@
 
 import kivy
-kivy.require('1.4.2')
+kivy.require('1.0.5')
 import os
 import sys
 from kivy.app import App
 from kivy.factory import Factory
 from kivy.lang import Builder, Parser, ParserException
 from kivy.graphics import Color, Ellipse, Rectangle, RoundedRectangle
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty, ListProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.codeinput import CodeInput
 from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scatter import Scatter
 
- 
+# from interface1 import Interface1App
+
+class ScatterWithImage(Scatter):
+    src = StringProperty("softboy.png")
 
 class Catalog(Screen):
     
-    
-    screen = ObjectProperty()
+    list = ['']
+    screen = ObjectProperty(None)
   
     def __init__(self, **kwargs):
         self._previously_parsed_text = ''
         super(Catalog, self).__init__(**kwargs)
-  
-        self.carousel = None
-    
-    def change_color(self, source):
-        Color(*BLEU)
-		
-    def disconnect(self):
-        self.manager.transition = SlideTransition(direction="right")
-        self.manager.current = 'interface1'
-        self.manager.get_screen('interface1')
 
-class Interface2App(App):
+        self.carousel = None
+
+
+    # def new_changes(self):
+    
+	
+    def on_enter(self):
+        self.listim = self.manager.list1
+        # self.grid = self.manager.gridtest
+        x = self.manager.x
+        print(x)
+		
+            
+        if x <= 2:
+            gridscreen = GridLayout(cols = x)
+            for y in self.listim:
+	
+                newimage= y+'.png'
+                gridscreen.add_widget(ScatterWithImage(src=newimage))
+        else:
+            gridscreen = GridLayout(rows = x-2)
+            for y in self.listim:
+
+                newimage= y+'.png'
+                gridscreen.add_widget(ScatterWithImage(src=newimage))
+
+        screen = self.manager.get_screen('interface2').screen
+        screen.clear_widgets()
+      
+        screen.add_widget(gridscreen)
+        # self.manager.list1 = self.listim
+        # self.manager.x = self.x
+		
+		
+	
+    def change_screen(self): 
+		
+
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'interface4'
+        self.manager.get_screen('interface4')
+        self.manager.list1 = self.listim
+        self.manager.x = self.x
+		
+		
+		
+    def backtointerface1(self):
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'interface2'
+        self.manager.get_screen('interface2')
+
+class Interface3App(App):
     '''The kivy App that runs the main root. All we do is build a catalog
     widget into the root.'''
 
     def build(self):
         screen = Catalog()
-        screen.name = 'interface2'
+        screen.name = 'interface3'
         return screen
     def on_pause(self):
         return True
 
 
 if __name__ == "__main__":
-    Interface2App().run()
+    Interface3App().run()
